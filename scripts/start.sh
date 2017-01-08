7a05258fbbb3d9f2c78a52f8dbb3b597c3be8efd
Wasin Thonkaew <haxpor@gmail.com>
2017-01-08 20:45:37 +0800

    Update startup script to support multiple lines of node result
    "node" process result from `grep` command can be in multiples.
    Updated startup script to take into account of this too. Thus only first line which is the server instance will be taken into used.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,9 @@
 
 # kill the existing process
 # select only line with "node" as it's the beginning of all
-RESULT=$(lsof -i :9000 | grep node | awk ' {print $2} ')
+# if `grep node` returns multiple lines, then only the first line will be used
+# (as it's mainly the process that server instance started it)
+RESULT=$(lsof -i :9000 | grep node | sed '1,1!d' | awk ' {print $2} ')
 
 # start the new process in the backround
 if [ "$RESULT" != "" ]
