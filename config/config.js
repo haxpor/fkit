531fe9028b48c3efa9152ba9ca386b4e7eb9b10a
Wasin Thonkaew <haxpor@gmail.com>
2017-02-12 06:38:05 +0800

    Fix url path to access resource serving by expressjs
    Actually we don't need prefix /fkit as it all depends on how we
    configured via dashboard.
    
    In case of production, the server has been configured to already
    include /fkit to point to local node program listening to incoming
    request. Thus no need for /fkit which could lead to 404.

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@
 
 var path = require('path');
 var root_path = path.resolve(__dirname + '/../');
-var prefix_path = '/fkit';
+var prefix_path = '/';
 var Baidut = require('baidut');
 
 // set environment for baidut
