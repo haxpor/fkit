ed5d8e42bc0e37a8385a97a11b178c3f4f3619d2
Wasin Thonkaew <haxpor@gmail.com>
2017-06-09 06:39:50 +0800

    Fix appid and key to use system environment variable

--- config/config.js
+++ config/config.js
@@ -9,8 +9,8 @@ var Baidut = require('baidut');
 
 // set environment for baidut
 Baidut.builder
-	.setAppId('2015063000000001')
-	.setKey('12345678')
+	.setAppId(process.env.FKIT_APPID)
+	.setKey(process.env.FKIT_KEY)
 	.build();
 
 /**
